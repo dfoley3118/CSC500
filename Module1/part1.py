246cece57741256b6136a773bf3f2414133267d0
Dennis Foley <dennis.foley@dennisf-ltmjmet.internal.salesforce.com>
2024-04-18 16:27:29 -0600

    cleanup of code

--- Module1/part1.py
+++ Module1/part1.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-input_str = input ("please enter a number: ")
+input_str = input ("Please enter a number: ")
 num1 = int(input_str)
-input_str = input ("please enter a second number: ")
+input_str = input ("Please enter a second number: ")
 num2 = int(input_str)
 
-num3 = num1 + num2
-print(num1, "+", num2, "=", num3)
-num3 = num1 - num2
-print(num1, "-", num2, "=", num3)
+print(num1, "+", num2, "=", num1 + num2)
+print(num1, "-", num2, "=", num1 - num2)
