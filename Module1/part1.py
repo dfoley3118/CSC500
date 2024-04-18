5b1dce1d31b00a504c99cf4b627be5c33080acc5
Dennis Foley <dennis.foley@dennisf-ltmjmet.internal.salesforce.com>
2024-04-17 20:57:18 -0600

    Code examples for Module1

---
+++ Module1/part1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+input_str = input ("please enter a number: ")
+num1 = int(input_str)
+input_str = input ("please enter a second number: ")
+num2 = int(input_str)
+
+num3 = num1 + num2
+print(str(num1) + " + " + str(num2) + " = " + str(num3))
+num3 = num1 - num2
+print(str(num1) + " - " + str(num2) + " = " + str(num3))
+
